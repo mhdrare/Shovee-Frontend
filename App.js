45b0b6523541a822a9567ee2da57b75b3f76c719
kevinmartinda <kevin36.k3@gmail.com>
2019-07-11 16:37:08 +0700

    basic onesignal setup

--- App.js
+++ App.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
 import AppNavigator from './src/routes/rootNavigator';
+import OneSignal from 'react-native-onesignal'
 
 export default class App extends Component {
+  constructor(properties) {
+    super(properties);
+    OneSignal.init("df4cae47-cd9d-4dd5-b97f-5f63593f39fb");
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+    OneSignal.configure(); 	// triggers the ids event
+  }
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log("Notification received: ", notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
+  }
+
   render() {
     return (
       <AppNavigator />
