32fd64ca4d73c0da4e956ddb59d3fa70ca6eabdd
andreferi3 <andreferi135@gmail.com>
2019-07-14 20:34:06 +0700

    update

--- src/screens/PurchaseHistories.js
+++ src/screens/PurchaseHistories.js
@@ -23,7 +23,7 @@ class ListHistories extends Component {
 					<View style={{backgroundColor: '#000', width: 100, height: 100, borderWidth: 0.5}}>
 						{
 							this.props.item.product.map(produk => (
-								produk > 0 ?  <Image key={produk[0]._id} style={{width: '100%', height: '100%'}} source={{ uri: produk[0].thumbnail}}/> : <Image key={produk._id} style={{width: '100%', height: '100%'}} source={{ uri: produk.thumbnail}}/>								
+								produk > 0 ?  <Image key={produk[0]._id} style={{width: '100%', height: '100%'}} source={{ uri: produk[0].thumbnail}}/> : <Image key={produk._id} style={{width: '100%', height: '100%'}} source={{ uri: produk.thumbnail}}/>
 							))
 						}
 					</View>
